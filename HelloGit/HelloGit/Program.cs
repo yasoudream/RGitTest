536c5ca12268b11b8b29554007f73923954f1ac7
yasoudream <634358229@qq.com>
2020-12-06 16:00:43 +0800

    xxxxooooo

--- HelloGit/HelloGit/Program.cs
+++ HelloGit/HelloGit/Program.cs
@@ -7,6 +7,9 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
